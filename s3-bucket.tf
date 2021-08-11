a2984e09169b607981e39247694887b091d201f4
rainbowrainer <marunachalam1@outlook.com>
2021-08-11 19:19:38 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,14 +2,11 @@ module "s3-bucket" {
   source  = "app.terraform.io/rainier-training/s3-bucket/aws"
   version = "2.2.0"
 
-  bucket = "hashicat-s3-bucket"
+  bucket = "${var.prefix}-hashicat-s3-bucket"
   acl    = "private"
 
   versioning = {
     enabled = true
   }
 
-  inputs = {
-    bucket_prefix    = "${var.prefix}"  
-  }
 }
